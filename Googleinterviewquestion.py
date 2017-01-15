f6088c7805e27626b6984e8044a693e5cdd4f6c4
Antonio Roldán Perez de la Rosa <Antonio7.9@hotmail.com>
2017-01-15 15:26:07 +0000

    Sample Interview question
    Given an array of n numbers and a sum output i, write a program that tells whether any of the possible sums between two elements in the list gives i as a solution.

--- Googleinterviewquestion.py
+++ Googleinterviewquestion.py
@@ -1,25 +1,11 @@
 import random
 
 class array():
-    def __init__(self, summ):
+    def __init__(self, summ, length):
         self.numbers_list = []
-        while True:
-            if self.numbers_list != 0:
-                self.numbers_list = [i for i in range(random.randrange(1, 10))]
-                break
-            else:
-                continue
-        self.c = 0
         self.sum = summ
-        for n in self.numbers_list:
-            self.random_modifier = self.c + random.randrange(1, len(self.numbers_list))
-            try:
-                self.numbers_list[self.c] = self.numbers_list[self.random_modifier]
-            except:
-                if self.numbers_list[self.c] > len(self.numbers_list):
-                    self.adjustment_value = len(self.numbers_list) - self.random_modifier
-                    self.random_modifier -= adjustment_value + random.randrange(1, adjustment_value)
-            self.c += 1
+        for i in range(length):
+            self.numbers_list.append(random.randrange(1, 10))
     def numbers_adder(self):
         threshold = len(self.numbers_list)
         count = 0
@@ -33,6 +19,7 @@ class array():
         print(self.numbers_list)
 
 
-numbers_list = array(8)
+numbers_list = array(8, 8)
 print(numbers_list.numbers_adder())
 numbers_list.getnumberslist()
+
