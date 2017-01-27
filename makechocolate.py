5eb43eb581f710c454f504b94cfc3bd1fcd00d65
Antonio Roldán Perez de la Rosa <Antonio7.9@hotmail.com>
2017-01-27 05:44:27 +0000

    Update makechocolate.py

--- makechocolate.py
+++ makechocolate.py
@@ -40,10 +40,8 @@ def make_chocolate(small_amount, big_amount, goal):
                 return small_sum # If the sum has equaled our goal, it is indiferent whether we choose one or the other variable
             elif small_sum > goal: # If the sum has exceeded our goal
                 return goal #Since the size is one, we can return goal as the result
-        elif big_sum == goal:
-            make_chocolate(small_amount, big_amount -1, goal) # We adjust the big_amount variable by - 1, to check for different combinations
-        elif big_sum > goal:
-            make_chocolate(small_amount, big_amount -1, goal) # We adjust the big_amount variable by - 1, to check for different combinations
+        elif big_sum >= goal:
+            make_chocolate(small_amount, big_amount -1, goal) # We adjust the big_amount variable by - 1, to check for different combinations      
         elif big_sum == 5:
             new_threshold_value = goal - big_sum # We adjust the new threshold value for the permutations function
             small_sums_list = permutations(small_amount, small_size, new_threshold_value) #Get all possible sums with our given small values
