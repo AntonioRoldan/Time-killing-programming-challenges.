99f3ab27d95569c30d8f8f398b1dd60379831afd
Antonio Roldán Perez de la Rosa <Antonio7.9@hotmail.com>
2017-01-27 05:36:09 +0000

    Create makebricks.py

---
+++ makebricks.py
@@ -0,0 +1,15 @@
+def make_bricks(small, big, goal):
+ small_size = 1
+ big_size = 5
+ formula = (small_size * small) + (big_size * big)
+ adjustment_value = formula - goal
+ if not formula % goal or not goal % formula:
+   return True
+ else:
+     if formula > goal:
+       if big_size % adjustment_value == 0 or small_size % adjustment_value == 0:
+         return True
+       else:
+         if adjustment_value % big_size == 0 or adjustment_value % small_size == 0:
+           return True
+ return False 
