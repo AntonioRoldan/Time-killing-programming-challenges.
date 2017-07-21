4c7efa9abbc6b19ef195342f749bbd308225d38f
Antonio Roldán Perez de la Rosa <Antonio7.9@hotmail.com>
2017-07-21 19:00:26 +0200

    Update makebricks.py

--- makebricks.py
+++ makebricks.py
@@ -9,7 +9,6 @@ def make_bricks(small, big, goal):
      if formula > goal:
        if big_size % adjustment_value == 0 or small_size % adjustment_value == 0:
          return True
-       else:
-         if adjustment_value % big_size == 0 or adjustment_value % small_size == 0:
-           return True
+       else if adjustment_value % big_size == 0 or adjustment_value % small_size == 0:
+         return True
  return False 
